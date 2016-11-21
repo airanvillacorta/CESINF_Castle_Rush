31cd41629284b0555a930044859489d9b7e05f04
Johan Plasencia <plasenciajohan@hotmail.com>
2016-11-21 16:40:16 +0000

    Intro added

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Intro : MonoBehaviour {
+    public MovieTexture movTexture;
+    void Start()
+    {
+        GetComponent<RawImage>().texture = movTexture as MovieTexture;
+        movTexture.Play();
+
+        StartCoroutine(Wait(movTexture.duration));
+    }
+
+
+
+    private IEnumerator Wait(float duration)
+    {
+        
+        yield return new WaitForSeconds(duration);
+
+        LoadMenu();
+    }
+
+    public void LoadMenu() {
+
+        Application.LoadLevel(1);
+    }
+}
+ 
+ 
+
+    
