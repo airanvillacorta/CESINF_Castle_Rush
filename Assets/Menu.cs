6048a7d0901e5e91a1549e596e6b7a7dabfd0adb
Johan Plasencia <plasenciajohan@hotmail.com>
2016-11-21 16:59:32 +0000

    intro added and menu

---
+++ Assets/Menu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject about;
+
+    public void Play() {
+        Application.LoadLevel(2);
+
+    }
+    public void Back() {
+
+        about.SetActive(false);
+    }
+
+    public void About()
+    {
+
+        about.SetActive(true);
+
+    }
+}
